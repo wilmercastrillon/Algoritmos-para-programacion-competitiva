e06a76d7ccab996b90acd5e62eee5736fc6356cf
Wilmer Emiro Castrillon Calderon <wilmercastrillon98@gmail.com>
2020-02-01 12:19:14 -0500

    Subset sum

--- Programacion dinamica/codigos/SubSetSum.cpp
+++ Programacion dinamica/codigos/SubSetSum.cpp
@@ -4,61 +4,58 @@
 
 using namespace std;
 typedef vector<int> vi;
-/*
-bool memo[5][50];
-void dp(vector<int> &num){
-    memset(memo, false, sizeof(memo));
 
-    for(int i = 0; i < num.size(); i++){
+bool memo[10][5];
+void dp(vector<int> &V){
+    memset(memo, false, sizeof(memo));
+    for(int i = 0; i < V.size(); i++){
         if(i){
-            for(int j = 1; j < 50; j++){
-                if(memo[i - 1][j]){
-                    memo[i][j] = true;
-                    memo[i][j + num[i]] = true;
+            for(int j = 1; j < 10; j++)
+                if(memo[j][i - 1]){
+                    memo[j][i] = true;
+                    memo[j + V[i]][i] = true;
                 }
-            }
         }
-        memo[i][num[i]] = true;
+        memo[V[i]][i] = true;
     }
 }
-*/
 
-int memo[10][50];
-int dp(int n, int pos, vector<int> &num){
-    if(memo[n][pos] != -1) return memo[n][pos];
+/*
+int memo[10][5];
+int dp(int n, int k, vector<int> &V){
+    if(memo[n][k] != -1) return memo[n][k];
     if(n == 0) return 1;
-    if(n < 0 || pos >= num.size())
-        return 0;
+    if(n < 0 || k >= V.size()) return 0;
 
-    if(dp(n-num[pos],pos+1,num)==1 || dp(n,pos+1,num)==1)
-        return memo[n][pos] = 1;
-    else return memo[n][pos] = 0;
+    if(dp(n-V[k],k+1,V)==1 || dp(n,k+1,V)==1)
+        return memo[n][k] = 1;
+    else return memo[n][k] = 0;
 }
-
+*/
 int main(){
     vi n;
     n.push_back(1);
     n.push_back(3);
     n.push_back(5);
-    //pre(n);
+    dp(n);
 
-    /*for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 20; j++){
+    /*for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 5; j++){
             printf("%3d", memo[i][j]);
         }
         printf("\n");
-    }
-    */
+    }*/
+
 
     int m;
 
     while(cin >> m){
-        //if(memo[n.size() - 1][m]) cout << "existe subconjunto\n";
-        //else cout << "no existe subconjunto\n";
-
-        memset(memo,-1,sizeof(memo));
-        if(dp(m,0,n) == 1) cout << "existe subconjunto\n";
+        if(memo[m][n.size() - 1]) cout << "existe subconjunto\n";
         else cout << "no existe subconjunto\n";
+
+        //memset(memo,-1,sizeof(memo));
+        //if(dp(m,0,n) == 1) cout << "existe subconjunto\n";
+        //else cout << "no existe subconjunto\n";
     }
 
     return 0;
