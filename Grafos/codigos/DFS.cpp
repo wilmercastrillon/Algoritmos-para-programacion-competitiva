9e3cfcfc5f6dec6dc46e989b6ce2e09299b8e1b7
Wilmer Emiro Castrillon Calderon <wilmercastrillon98@gmail.com>
2020-05-20 22:38:06 -0500

    Ajustes de dp y grafos

---
+++ Grafos/codigos/DFS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef vector<int> vi;
+
+vector<vector<int>> grafo(10);
+
+void construir_grafo(){
+    grafo[5].push_back(4);
+    grafo[1].push_back(2);
+    grafo[2].push_back(3);
+    grafo[1].push_back(3);
+    grafo[1].push_back(5);
+}
+
+bool visitado[10];
+
+void dfs(int nodo){
+    visitado[nodo] = true;
+    for(int i = 0; i < grafo[nodo].size(); i++){
+        if(!visitado[grafo[nodo][i]]){
+            dfs(grafo[nodo][i]);
+        }
+    }
+}
+
+int main(){
+    int n = 5;
+    construir_grafo();
+    dfs(1);
+}
